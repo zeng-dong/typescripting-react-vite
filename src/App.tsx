8d8ac853177e64d77196f068240fbc8876327019
Dong Zeng <zeng-dong@hotmail.com>
2022-12-07 14:09:19 -0600

    navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Route, Routes } from "react-router-dom";
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Store } from "./pages/Store";
+import { Navbar } from "./components/Navbar";
 
 function App() {
 	return (
-		<Container>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/store" element={<Store />} />
-				<Route path="/about" element={<About />} />
-			</Routes>
-		</Container>
+		<>
+			<Navbar />
+			<Container className="mb-4">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/store" element={<Store />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</Container>
+		</>
 	);
 }
 
