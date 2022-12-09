7ea6bd05e1c4f15850efab5a55793dd518c71349
Dong Zeng <zeng-dong@hotmail.com>
2022-12-08 18:02:09 -0600

    style row

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,7 +5,7 @@ export function Store() {
 	return (
 		<>
 			<h1>Store</h1>
-			<Row>
+			<Row md={2} xs={1} lg={3} className="g-3">
 				{storeItems.map((item) => (
 					<Col key={item.id}>{JSON.stringify(item)}</Col>
 				))}
