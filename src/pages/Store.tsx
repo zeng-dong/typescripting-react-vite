4bdcce37ea82f503821dae1b6d75602294d345a4
Dong Zeng <zeng-dong@hotmail.com>
2022-12-08 18:14:33 -0600

    card img

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,4 +1,5 @@
 import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/StoreItem";
 import storeItems from "../data/items.json";
 
 export function Store() {
@@ -7,7 +8,9 @@ export function Store() {
 			<h1>Store</h1>
 			<Row md={2} xs={1} lg={3} className="g-3">
 				{storeItems.map((item) => (
-					<Col key={item.id}>{JSON.stringify(item)}</Col>
+					<Col key={item.id}>
+						<StoreItem {...item} />
+					</Col>
 				))}
 			</Row>
 		</>
