490a0cbdb44b76bf7370b4558b0761cc65144187
Dong Zeng <zeng-dong@hotmail.com>
2022-12-08 18:00:35 -0600

    1st version of store items

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,15 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+
 export function Store() {
-	return <h1>Store</h1>;
+	return (
+		<>
+			<h1>Store</h1>
+			<Row>
+				{storeItems.map((item) => (
+					<Col key={item.id}>{JSON.stringify(item)}</Col>
+				))}
+			</Row>
+		</>
+	);
 }
