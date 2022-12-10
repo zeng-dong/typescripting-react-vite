af457cf8143e874abbf5ffa3aca6366c88a9d203
Dong Zeng <zeng-dong@hotmail.com>
2022-12-10 11:14:41 -0600

    fix space between

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -18,7 +18,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 			/>
 
 			<Card.Body className="d-flex flex-column">
-				<Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+				<Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
 					<span className="fs-2">{name}</span>
 					<span className="ms-2 text-muted">{price}</span>
 				</Card.Title>
