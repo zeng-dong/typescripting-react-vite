9f599e89aca6ab42c2f48c2f9cd6b32b0a2a86d1
Dong Zeng <zeng-dong@hotmail.com>
2022-12-11 12:39:05 -0600

    simple context

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+
+const ShoppingCartContext = createContext({});
+
+export function useShoppingCart() {
+	return useContext(ShoppingCartContext);
+}
