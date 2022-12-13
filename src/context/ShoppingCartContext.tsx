5043a2b9fe1a12247267b7ae3c583b6b7e880d2f
Dong Zeng <zeng-dong@hotmail.com>
2022-12-12 20:12:50 -0600

    increaseCartQuantity

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -25,6 +25,26 @@ export function useShoppingCart() {
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 	const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
+	function getItemQuantity(id: number) {
+		return cartItems.find((x) => x.id === id)?.quantity || 0;
+	}
+
+	function increaseCartQuantity(id: number) {
+		setCartItems((currentItems) => {
+			if (currentItems.find((item) => item.id === id) == null) {
+				return [...currentItems, { id, quantity: 1 }];
+			} else {
+				return currentItems.map((item) => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity + 1 };
+					} else {
+						return item;
+					}
+				});
+			}
+		});
+	}
+
 	return (
 		<ShoppingCartContext.Provider value={{}}>
 			{children}
