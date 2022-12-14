718095ec116cb012f7085b1c6fc0d867cd40cca7
Dong Zeng <zeng-dong@hotmail.com>
2022-12-13 21:03:44 -0600

    decreaseCartQuantity

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -45,6 +45,22 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
 		});
 	}
 
+	function decreaseCartQuantity(id: number) {
+		setCartItems((currentItems) => {
+			if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+				return currentItems.filter((item) => item.id !== id);
+			} else {
+				return currentItems.map((item) => {
+					if (item.id === id) {
+						return { ...item, quantity: item.quantity - 1 };
+					} else {
+						return item;
+					}
+				});
+			}
+		});
+	}
+
 	function removeFromCart(id: number) {
 		setCartItems((currentItems) => {
 			return currentItems.filter((item) => item.id !== id);
