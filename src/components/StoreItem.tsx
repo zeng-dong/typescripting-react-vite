4d6ebe7aab03bee093435336342cbab4bf110502
Dong Zeng <zeng-dong@hotmail.com>
2022-12-14 20:38:20 -0600

    add to cart button onclick

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -9,7 +10,13 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-	const quantity = 1;
+	const {
+		getItemQuantity,
+		increaseCartQuantity,
+		decreaseCartQuantity,
+		removeFromCart,
+	} = useShoppingCart();
+	const quantity = getItemQuantity(id);
 
 	return (
 		<Card className="h-100">
@@ -29,7 +36,12 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 				</Card.Title>
 				<div className="mt-auto">
 					{quantity === 0 ? (
-						<Button className="w-100">+ Add to Cart</Button>
+						<Button
+							className="w-100"
+							onClick={() => increaseCartQuantity(id)}
+						>
+							+ Add to Cart
+						</Button>
 					) : (
 						<div
 							className="d-flex align-items-center flex-column"
